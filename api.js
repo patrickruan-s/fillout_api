454ce793170329f77614f1d1e190ee06aa70ef1f
Patrick Ruan <patrickruan@Patricks-MacBook-Air-2.local>
2024-03-07 14:25:23 -0600

    initial filter code

--- api.js
+++ api.js
@@ -4,8 +4,33 @@ api.use(express.json());
 
 const port = process.env.PORT || 3000;
 
-async function getFilloutData(params) {
-  debugger
+const compareToFilters = (response, filters) => {
+  for (let i = 0; i < filters.length; i++) {
+    const filter = filters[i];
+    const condition = filter.condition;
+    const id = filter.id;
+    const value = filter.value;
+
+    const question = response.questions.find(question => question.id == id);
+    if ((condition == 'equals' && question.value == value ) || (condition == 'does_not_equal' && question.value != value)
+    || (condition == 'greater_than' && question.value > value) || (condition == 'less_than' && question.value < value)) {
+      continue;
+    } else {
+      return false;
+    }
+  }
+}
+
+const filterResponse = (data, filters) => {
+  if (filters.length == 0 || filters == null) { return data };
+  var newResponses = [];
+  data.responses.forEach((response) => {
+    if(compareToFilters(response, filters)) { newReponses.push(response) };
+  });
+  data.responses = newResponses;
+}
+
+async function getFilloutData(filters) {
     const token = 'sk_prod_TfMbARhdgues5AuIosvvdAC9WsA5kXiZlW8HZPaRDlIbCpSpLsXBeZO7dCVZQwHAY3P4VSBPiiC33poZ1tdUj2ljOzdTCCOSpUZ_3912';
     const url = 'https://api.fillout.com/v1/api/forms/cLZojxk94ous/submissions';
     let response = await fetch(url, {
@@ -15,13 +40,14 @@ async function getFilloutData(params) {
           "Authorization": `Bearer ${token}`
         }
     });
-    const data = await response.json();
-    return data;
+    let data = await response.json();
+    ret_data = filterResponse(data, filters);
+    return ret_data;
 }
 
-api.get('/status', async (request, response) => {
+api.get('/cLZojxk94ous/filteredResponses', async (request, response) => {
     try {
-        responseFromFillout = await getFilloutData(request.params);
+        responseFromFillout = await getFilloutData(request.query.filters);
         response.send(responseFromFillout);
       } catch (error) {
         console.error(error);
